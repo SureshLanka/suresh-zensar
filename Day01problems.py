cfe8e787a41495955bfe5119ea41f586cb873657
SureshLanka <103917531+SureshLanka@users.noreply.github.com>
2022-05-05 13:58:05 +0530

    Update Day01problems.py

--- Day01problems.py
+++ Day01problems.py
@@ -34,6 +34,22 @@ else:
     print("not")
 
 print("-"*40)
+#greety problem
+count=0
+for i in range(1,501):
+    if i%3==1:
+        a=2*((i-1)//3)
+        if a%3==1:
+            b=2 * ((a - 1) // 3)
+            if b%3==1:
+                c=2*((b-1)//3)
+                if c%3==0:
+                    print(i,end=',')
+                    count+=1
+                    if count==5:
+                        break
+
+
 
 #fibonacci series
 print("-" * 40)
